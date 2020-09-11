f9ed33584e63c010fa019dd05967e802979d9e5d
Robert Neijmeijer <Robert.Neijmeijer@ahold.com>
2020-09-11 23:52:22 +0200

    App done

--- app/src/main/java/com/example/madlevel2task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task2/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.madlevel2task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel2task2.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +26,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun initView() {
 
+        questions.add(Question("Should be true",true))
+        questions.add(Question("Should be false",false))
+        questions.add(Question("Should be true",true))
+        questions.add(Question("Should be false",false))
+
+
         binding.rvQuestions.layoutManager =
             LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
         binding.rvQuestions.adapter = questionAdapter
@@ -39,7 +47,7 @@ class MainActivity : AppCompatActivity() {
     private fun createItemTouchHelper(): ItemTouchHelper {
 
 
-        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
 
             override fun onMove(
                 recyclerView: RecyclerView,
@@ -51,10 +59,23 @@ class MainActivity : AppCompatActivity() {
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 val position = viewHolder.adapterPosition
-                questions.removeAt(position)
+                if(checkCorrect(questions.get(position), direction)) {
+                    questions.removeAt(position)
+                    if(questions.size == 0) onComplete()
+                } else{
+                    Snackbar.make(binding.tvAnswer,R.string.incorrect_answer,Snackbar.LENGTH_SHORT).show()
+                }
                 questionAdapter.notifyDataSetChanged()
             }
         }
         return ItemTouchHelper(callback)
+    }
+
+    private fun checkCorrect(question: Question,direction: Int): Boolean{
+        return question.correctAnswer && direction == 8 || !question.correctAnswer && direction == 4
+    }
+
+    private fun onComplete(){
+        Snackbar.make(binding.tvAnswer,R.string.congratulations,Snackbar.LENGTH_SHORT).show()
     }
 }
